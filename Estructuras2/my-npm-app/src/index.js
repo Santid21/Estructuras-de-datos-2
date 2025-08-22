586cb4ffedf0f8fdb4ec94eddf6b1807c645828b
Santid21 <sduque04@gmail.com>
2025-08-22 09:22:29 -0500

    Subida inicial de archivos

---
+++ Estructuras2/my-npm-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FistApp from './FistApp';
+import App from './App';
+
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+  <React.StrictMode>
+    <FistApp/>
+    <App/>
+  </React.StrictMode>
+)
