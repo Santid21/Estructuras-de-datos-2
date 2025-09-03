0f32d8a47c5f0d610372ff830e9b377b5f529fa4
Santid21 <sduque04@gmail.com>
2025-09-03 14:37:59 -0500

    Quitar carpeta challenger 4 solo del remoto en challenger-6

--- challenger 4/my-npm-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ComponentApp } from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ComponentApp />);
