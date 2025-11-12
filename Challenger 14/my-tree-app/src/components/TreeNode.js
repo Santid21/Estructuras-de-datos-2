afe80ea20c5705baa7b2036edb2df84580568536
Santiago Duque <138895787+Santid21@users.noreply.github.com>
2025-11-12 14:40:25 -0500

    Delete Challenger 14/my-tree-app directory

--- Challenger 14/my-tree-app/src/components/TreeNode.js
+++
@@ -1,7 +0,0 @@
-export default class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
--- Challenger 14/my-tree-app/src/data/sampleData.js
+++
@@ -1,1 +0,0 @@
-export const numbers = [50, 30, 70, 20, 40, 60, 80];
